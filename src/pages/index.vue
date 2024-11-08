0939b9ab86b873ce8d5cc59c3b06f0371269efdb
qq320339793 <320339793@qq.com>
2024-11-08 22:25:23 +0800

    add index page

---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <h1 text-center>
+      Home
+      <div i-mdi:database h-1em w-1em />
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
